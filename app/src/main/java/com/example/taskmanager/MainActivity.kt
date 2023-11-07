e84023fa680474babf755e29f6c79da8e89d6f2e
courtlynboykin <courtlynb@google.com>
2023-11-07 17:00:31 -0500

    Changed column to Box for TaskImage function, removed comments, optimized imports, and refactored code

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -34,9 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TaskMessage(heading = stringResource(R.string.tasks_completed), message = stringResource(
-                        R.string.nice_work
-                    )
+                    TaskMessage(
+                        heading = stringResource(R.string.tasks_completed),
+                        message = stringResource(
+                            R.string.nice_work
+                        )
                     )
                 }
             }
@@ -46,13 +46,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskMessage(heading: String, message: String, modifier: Modifier = Modifier) {
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .padding(
-                horizontal = 20.dp,
-                vertical = 250.dp
-            )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = heading,
@@ -73,24 +68,23 @@ fun TaskMessage(heading: String, message: String, modifier: Modifier = Modifier)
 @Composable
 fun TaskImage(heading: String, message: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.ic_task_completed)
-    Box {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(
+                horizontal = 100.dp,
+                vertical = 200.dp
+            )
+    ) {
         Image(
             painter = image,
             contentDescription = null,
             modifier = Modifier
-                .padding(
-                    horizontal = 100.dp,
-                    vertical = 250.dp
-        )
         )
         TaskMessage(
             heading = heading,
-            message = message,
-            modifier = Modifier
-                .padding(
-                    horizontal = 100.dp,
-                    vertical = 250.dp
-                )
+            message = message
         )
     }
 }
